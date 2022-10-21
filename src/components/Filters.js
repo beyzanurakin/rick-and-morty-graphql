63da67752d44e170d8fcff02e7d20504bee0a6b3
beyzanurakin <akinbeyzanur@gmail.com>
2022-10-21 17:32:30 +0300

    Add Filters component

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,7 +4,24 @@ import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
 
 const Filters = ({ handleClose, show, filteredName, setFilteredName }) => {
-  return <div>Filters</div>
+  const handleChange = (e) => {
+    e.preventDefault()
+    setFilteredName(e.target.value)
+  }
+  return (
+    <Modal show={show} onHide={handleClose} className='fs-3'>
+      <Modal.Header closeButton>Filter</Modal.Header>
+      <button onClick={() => setFilteredName('')}>Clean Filter</button>
+      <Modal.Body>
+        <Form onChange={(e) => setFilteredName(e.target.value)}>
+          <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <Form.Check type='radio' name='group' value='rick' label='Rick' />
+            <Form.Check type='radio' name='group' value='morty' label='Morty' />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  )
 }
 
 export default Filters
